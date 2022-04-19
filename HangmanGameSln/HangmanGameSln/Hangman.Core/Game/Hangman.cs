a65e9321ce85d876ba5391556eea3acbc552d10c
SimamkeleSefalane <102727091+SimamkeleSefalane@users.noreply.github.com>
2022-04-19 15:24:41 +0200

    Added guessing code

--- HangmanGameSln/HangmanGameSln/Hangman.Core/Game/Hangman.cs
+++ HangmanGameSln/HangmanGameSln/Hangman.Core/Game/Hangman.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using HangmanRenderer.Renderer;
 
 namespace Hangman.Core.Game
@@ -16,6 +16,31 @@ namespace Hangman.Core.Game
         {
             _renderer.Render(5, 5, 6);
 
+            string[] wordList = new string[20] {"alpha","bravo","charlie","delta", "echo","foxtrot","golf","hotel","india","juliett",
+            "killo", "lima","mike","november","oscar","papa","romeo","sierra","tango","uniform"};
+
+            Random randWord = new Random();
+            var index = randWord.Next(0, 19);
+            string unknownWord = wordList[index];
+            char[] guess = new char[unknownWord.Length];
+            Console.WriteLine("Please enter your guess");
+
+            for (int w = 0; w < unknownWord.Length; w++)
+                guess[w] = '_';
+
+            while (true)
+            {
+
+                char playerGuess = char.Parse(Console.ReadLine());
+                for (int l = 0; l < unknownWord.Length; l++)
+                    guess[l] = '_';
+
+
+
+
+
+
+            }
             Console.SetCursorPosition(0, 13);
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.Write("Your current guess: ");
@@ -26,7 +51,8 @@ namespace Hangman.Core.Game
 
             Console.Write("What is your next guess: ");
             var nextGuess = Console.ReadLine();
-        }
-
+        } 
+       
     }
 }
+
