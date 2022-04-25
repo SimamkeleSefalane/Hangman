df0784f68fb246ea6e9ba586fec5843978d4bb05
SimamkeleSefalane <102727091+SimamkeleSefalane@users.noreply.github.com>
2022-04-25 15:31:07 +0200

    Added completed code

--- HangmanGameSln/HangmanGameSln/Hangman.Core/Game/Hangman.cs
+++ HangmanGameSln/HangmanGameSln/Hangman.Core/Game/Hangman.cs
@@ -6,6 +6,7 @@ namespace Hangman.Core.Game
     public class HangmanGame
     {
         private GallowsRenderer _renderer;
+        private int _playsLeft = 6;
 
         public HangmanGame()
         {
@@ -14,7 +15,9 @@ namespace Hangman.Core.Game
 
         public void Run()
         {
-            _renderer.Render(5, 5, 6);
+            string correctWordGuessed = string.Empty;
+
+            _renderer.Render(5, 5, _playsLeft);
 
             string[] wordList = new string[20] {"alpha","bravo","charlie","delta", "echo","foxtrot","golf","hotel","india","juliett",
             "killo", "lima","mike","november","oscar","papa","romeo","sierra","tango","uniform"};
@@ -23,34 +26,58 @@ namespace Hangman.Core.Game
             var index = randWord.Next(0, 19);
             string unknownWord = wordList[index];
             char[] guess = new char[unknownWord.Length];
-            Console.WriteLine("Please enter your guess");
 
             for (int w = 0; w < unknownWord.Length; w++)
-                guess[w] = '_';
+                guess[w] = '-';
 
-            while (true)
+            while (_playsLeft > 0 && _playsLeft <= 6)
             {
 
-                char playerGuess = char.Parse(Console.ReadLine());
-                for (int l = 0; l < unknownWord.Length; l++)
-                    guess[l] = '_';
+                Console.SetCursorPosition(0, 13);
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write("Your current guess: ");
+                Console.WriteLine(guess);
+                Console.WriteLine("--------------");
+                Console.SetCursorPosition(0, 15);
 
+                Console.ForegroundColor = ConsoleColor.Green;
 
+                Console.Write("What is your next guess: ");
+                var nextGuess = char.Parse(Console.ReadLine());
 
+                bool rightGuess = false;
+                {
+                    for (int l = 0; l < guess.Length; l++)
+                    {
+                        if (nextGuess == unknownWord[l])
+                        {
+                            guess[l] = nextGuess;
+                            rightGuess = true;
+                        }
+                    }
+                    if(!rightGuess)
+                    {
+                        _playsLeft--;
+                        _renderer.Render(5, 5, _playsLeft);
 
+                    }
+
+                }
+                correctWordGuessed = new string(guess);
+                if (correctWordGuessed == unknownWord)
+                {
+                    Console.SetCursorPosition(0, 20);
+                    Console.WriteLine("Hooray!!! You survived");
+                }
 
 
             }
-            Console.SetCursorPosition(0, 13);
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.Write("Your current guess: ");
-            Console.WriteLine("--------------");
-            Console.SetCursorPosition(0, 15);
-
-            Console.ForegroundColor = ConsoleColor.Green;
-
-            Console.Write("What is your next guess: ");
-            var nextGuess = Console.ReadLine();
+            if (correctWordGuessed != unknownWord)
+            {
+                Console.SetCursorPosition(0, 20);
+                Console.WriteLine("Sorry! You died");
+                Console.WriteLine("The word was: " + unknownWord);
+            }
         } 
        
     }
